098f3e01d1af85858f5edb51f748b005087afaaf
Gerold Katzinger <gerold@katzinger.info>
2017-03-04 12:34:24 +0100

    added au rclone package

---
+++ automatic/rclone/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+$zip_path = if ((Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne 'true') {
+         Write-Host "Installing 64 bit version"; gi "$toolsPath\*_x64.zip"
+} else { Write-Host "Installing 32 bit version"; gi "$toolsPath\*_x32.zip" }
+
+
+
+$packageArgs = @{
+    PackageName  = 'rclone'
+    FileFullPath = $zip_path
+    Destination  = $toolsPath
+}
+Get-ChocolateyUnzip @packageArgs
+rm $zip_path -ea 0
