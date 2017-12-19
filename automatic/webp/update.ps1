6830243b099177bcc9228b74f560773810bc24ad
Gerold Katzinger <gerold@katzinger.info>
2017-12-19 22:32:43 +0100

    update webp au script and package

--- automatic/webp/update.ps1
+++ automatic/webp/update.ps1
@@ -8,7 +8,7 @@ function global:au_SearchReplace {
             "(?i)(^\s*packageName\s*=\s*)('.*')"  = "`$1'$($Latest.PackageName)'"
         }
 
-        ".\legal\VERIFICATION.txt" = @{
+        ".\tools\VERIFICATION.txt" = @{
           "(?i)(\s+x32:).*"            = "`${1} $($Latest.URL32)"
           "(?i)(\s+x64:).*"            = "`${1} $($Latest.URL64)"
           "(?i)(checksum32:).*"        = "`${1} $($Latest.Checksum32)"
