932b52fcaf8e879dee33ec8bb543da21c9dd77fe
Gerold Katzinger <g.katzinger@conova.com>
2017-12-20 18:08:08 +0100

    add windowsspyblocker package

---
+++ automatic/windowsspyblocker/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+$desktopPath = [Environment]::GetFolderPath("Desktop")
+$lnkPath = $desktopPath + "\WindowsSpyBlocker.lnk"
+$exePath = Join-Path $toolsPath "WindowsSpyBlocker.exe"
+
+Install-ChocolateyShortcut -shortcutFilePath $lnkPath -targetPath $exePath
+
---
+++ automatic/windowsspyblocker/update.ps1
@@ -0,0 +1,39 @@
+import-module au
+
+$releases = 'https://github.com/crazy-max/WindowsSpyBlocker/releases'
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\VERIFICATION.txt" = @{
+          "(?i)(\s+x64:).*"            = "`${1} $($Latest.URL64)"
+          "(?i)(checksum64:).*"        = "`${1} $($Latest.Checksum64)"
+          "(?i)(Get-RemoteChecksum).*" = "`${1} $($Latest.URL64)"
+        }
+    }
+}
+function global:au_BeforeUpdate {
+
+    Get-RemoteFiles -NoSuffix -Purge 
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    
+    $re = '.+\.exe$'
+    $url   = "https://github.com$($download_page.links | ? href -match $re | select -First 1 -expand href)"
+
+
+    $version  = $url -split '/' | select -Last 1 -Skip 1
+
+    @{
+        Version      = $version
+        URL64        = $url
+    }
+}
+
+try {
+    update -ChecksumFor none
+} catch {
+    $ignore = 'Unable to connect to the remote server'
+    if ($_ -match $ignore) { Write-Host $ignore; 'ignore' }  else { throw $_ }
+}
